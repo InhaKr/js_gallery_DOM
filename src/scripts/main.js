fe80d23b48d9b324e14a773537d2c499b8b4095a
InhaKr <inkr3891@gmail.com>
2025-09-16 11:30:39 +0300

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,11 @@
 'use strict';
+
+const ul = document.querySelector('#thumbs');
+const largeImg = document.querySelector('.gallery__large-img');
+
+ul.addEventListener('click', (e) => {
+  e.preventDefault();
+
+  const mainScreen = e.target.closest('.list-item__link');
+  largeImg.src = mainScreen.href;
+});
