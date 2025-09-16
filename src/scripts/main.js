06632606843d2fa7d05e915a56ddbcd5af79c31c
InhaKr <inkr3891@gmail.com>
2025-09-16 11:57:01 +0300

    Solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -4,8 +4,14 @@ const ul = document.querySelector('#thumbs');
 const largeImg = document.querySelector('.gallery__large-img');
 
 ul.addEventListener('click', (e) => {
-  e.preventDefault();
 
   const mainScreen = e.target.closest('.list-item__link');
+
+  if (!mainScreen) {
+    return;
+  }
+
+  e.preventDefault();
+
   largeImg.src = mainScreen.href;
 });
